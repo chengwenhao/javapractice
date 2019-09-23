1642bbe60f9688aec0fe3586cdffa0ca684dafcc
chengwenhao <1090610426@qq.com>
2019-09-23 23:15:45 +0800

    Initial commit

---
+++ src/demo/Demo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("sds");
+        System.out.println("sdsdsdsds");
+
+        String s = "Java";
+        boolean b = s.startsWith("J");
+        System.out.println(b);
+    }
+}
