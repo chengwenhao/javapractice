1cb6417ae16183d4dfcb6df6f8b7c2e1a4f0c487
chengwenhao <1090610426@qq.com>
2019-09-25 22:42:44 +0800

    localDate

---
+++ src/localdate/LocaldateTest.java
@@ -0,0 +1,11 @@
+package localdate;
+
+import java.time.LocalDate;
+
+public class LocaldateTest {
+
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
