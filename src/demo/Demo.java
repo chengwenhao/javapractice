14c42d8c37c4101c7865b0522cb96e7a01bda59b
chengwenhao <1090610426@qq.com>
2019-10-08 22:37:37 +0800

    localDate

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,9 +3,6 @@ package demo;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("sds");
-        System.out.println("sdsdsdsds");
-
         String s = "Java";
         boolean b = s.startsWith("J");
         System.out.println(b);
