366ecb3cbc234aca3933839e516fd290e3d045b8
chengwh <1090610426@qq.com>
2019-11-01 17:11:46 +0800

    map java8新增方法

--- src/main/java/map/MapTestJava8.java
+++ src/main/java/map/MapTestJava8.java
@@ -8,20 +8,28 @@ import java.util.*;
 public class MapTestJava8 {
 
     public static void main(String[] args) {
-        Map<String, ArrayList<Integer>> map = new HashMap<>();
-        List<String> stringList = Arrays.asList("1", "2", "2", "3", "5", "6", "6");
         //1.map.putIfAbsent();
-        for (int i = 0;i < stringList.size() ; i++) {
-            List<Integer> o = map.putIfAbsent(stringList.get(i), new ArrayList<Integer>(1));
-            System.out.println(o);
-            System.out.println(map);
+        Map<String, ArrayList<Integer>> map1 = new HashMap<>();
+        List<String> stringList1 = Arrays.asList("1", "2", "2", "3", "5", "6", "6");
+        for (int i = 0;i < stringList1.size() ; i++) {
+            map1.putIfAbsent(stringList1.get(i), new ArrayList<Integer>(1));
+            map1.get(stringList1.get(i)).add(i);
         }
-
-
+        System.out.println(map1);
         //2.map.compute();
-
+        Map<String, ArrayList<Integer>> map2 = new HashMap<>();
+        List<String> stringList2 = Arrays.asList("1", "2", "2", "3", "5", "6", "6");
+        for (int i = 0; i < stringList2.size();i++){
+            map2.compute(stringList2.get(i),(k,v) -> v==null?new ArrayList<>(1):v).add(i);
+        }
+        System.out.println(map2);
         //3.map.computeIfAbsent();
-
+        Map<String, ArrayList<Integer>> map3 = new HashMap<>();
+        List<String> stringList3 = Arrays.asList("1", "2", "2", "3", "5", "6", "6");
+        for (int i = 0; i < stringList3.size();i++){
+            map3.computeIfAbsent(stringList3.get(i),(k)->new ArrayList<>(1)).add(i);
+        }
+        System.out.println(map3);
         //4.map.computeIfPresent();
 
         //5.map.merge();
