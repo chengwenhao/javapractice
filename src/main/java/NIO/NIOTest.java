0cb731df930bf61bf4389dd4ec0b7907ad35e72c
chengwenhao <1090610426@qq.com>
2019-12-02 22:24:42 +0800

    NIO使用

--- src/main/java/NIO/NIOTest.java
+++ src/main/java/NIO/NIOTest.java
@@ -1,37 +1,52 @@
 package NIO;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class NIOTest {
 
-    public static void main(String[] args) {
-        List<Integer> objects = new ArrayList<>();
-        for (int i = 0;i<1000000;i++){
-            objects.add(i);
-        }
-//        long l = System.currentTimeMillis();
-//        System.out.println(l);
-//        List<Object> objects1 = new ArrayList<>();
-//        for (Integer i : objects) {
-//            if (i==10 || i == 100 || i == 111){
-//                objects1.add(i);
-//            }
-//        }
-//        System.out.println(objects1);
-//        long l1 = System.currentTimeMillis();
-//        System.out.println(l1);
-//        System.out.println(l1-l);
-
+    public static void main(String[] args) throws IOException {
         long l = System.currentTimeMillis();
         System.out.println(l);
-        List<Integer> collect = objects.parallelStream().filter(i -> i == 10 || i == 100 || i == 111).collect(Collectors.toList());
-        System.out.println(collect);
+        String rFile = "E:\\IdeaProjects\\javapracticeFile\\NIOtest\\test1.JPG";
+        String wFile = "E:\\IdeaProjects\\javapracticeFile\\NIOtest\\test3.JPG";
+        String wFile1 = "E:\\IdeaProjects\\javapracticeFile\\NIOtest\\test4.JPG";
+        //NIO
+        FileChannel fcin = new FileInputStream(rFile).getChannel();
+        FileChannel fcout = new FileOutputStream(wFile).getChannel();
+        //1.方法一
+        /*ByteBuffer buff = ByteBuffer.allocate(1024);
+        while(true){
+            buff.clear();
+            if((fcin.read(buff)) == -1){
+                break;
+            }
+            buff.flip();
+            fcout.write(buff);
+        }*/
+
+        //2.方法二
+       /* fcin.transferTo(0, fcin.size(), fcout);
         long l1 = System.currentTimeMillis();
         System.out.println(l1);
-        System.out.println(l1 -l);
+        System.out.println(l1-l);*/
 
+
+        //IO
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(new File(rFile)));
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(wFile1)));
+        byte[] bytes = new byte[1024];
+        int i = 0;
+        while (i != -1){
+            i = in.read(bytes);
+            out.write(bytes);
+        }
+        in.close();
+        out.close();
+        long l1 = System.currentTimeMillis();
+        System.out.println(l1);
+        System.out.println(l1-l);
     }
 
 
