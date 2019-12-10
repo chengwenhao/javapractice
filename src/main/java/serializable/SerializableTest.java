bd1695807118823bae0fcd81cf6dd1de40f0e391
chengwenhao <1090610426@qq.com>
2019-12-10 22:03:41 +0800

    serializable的使用示例

--- src/main/java/serializable/SerializableTest.java
+++ src/main/java/serializable/SerializableTest.java
@@ -2,10 +2,52 @@ package serializable;
 
 import java.io.Serializable;
 
-public class SerializableTest implements Serializable{
+public class SerializableTest{
 
     public static void main(String[] args) {
-        
+
     }
 
+}
+
+class User implements Serializable{
+
+    private static long serialVersionUID = 1L;
+
+    private String userName;
+    private String userNo;
+    private String age;
+    private String sex;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(String userNo) {
+        this.userNo = userNo;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
