22e821424c4eae50795b0b4b3c2876a15a7971c9
chengwenhao <1090610426@qq.com>
2019-12-19 22:43:48 +0800

    serializable的使用示例

--- src/main/java/concurrenthashmap/ConcurrentHashMapTest.java
+++ src/main/java/concurrenthashmap/ConcurrentHashMapTest.java
@@ -1,8 +1,15 @@
 package concurrenthashmap;
 
+import java.util.*;
+
 public class ConcurrentHashMapTest {
 
     public static void main(String[] args) {
+
+        Map<Object, Object> map = Collections.synchronizedMap(new HashMap<>());
+
+        ArrayList<Object> list = new ArrayList<>();
+        Iterator<Object> iterator = list.iterator();
 
     }
 }
